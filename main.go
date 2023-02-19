a08dc13f83f719c3a6ff3cc403320cfa7a826195
Beto <b3t0@moon>
2023-02-19 08:59:28 -0600

    basic windows with go and glfw

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+const (
+	width  = 500
+	height = 500
+)
+
+func main() {
+	runtime.LockOSThread()
+
+	window := initGlfw()
+	defer glfw.Terminate()
+
+	for !window.ShouldClose() {
+		// TODO
+	}
+}
+
+// initGlfw initializes glfw and return a Window to use.
+func initGlfw() *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width, height, " EY yoo", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	return window
+}
